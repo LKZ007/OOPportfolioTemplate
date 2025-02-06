99873b430982ef80feb8b6ab9fb45ec2b7d40a79
LKZ007 <l.kibale-zoko3989@student.leedsbeckett.ac.uk>
2025-02-06 11:21:50 +0000

    weel22

--- WeeklyExercises/2DataAndExpressions/StudentGrade.java
+++ WeeklyExercises/2DataAndExpressions/StudentGrade.java
@@ -6,9 +6,22 @@ public class StudentGrade
         System.out.println("==          Student Points          ==");
         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
         System.out.println("Name            Lab     Bonus   Total");
-        System.out.println("Joe             43      7       50");
-        System.out.println("William         50      8       58");
-        System.out.println("Mary Sue        39      10      49");
+        System.out.println("Ein             43      7       50");
+        System.out.println("Elliot          50      8       58");
+        System.out.println("Olidura         39      10      49");
+        System.out.println("George          25      4       36");
     }
+import java.util.Scanner;
+    public class Average
+    {
+        public static void main(String[] args)
+        {
+            int val1, val2, val3;
+            double average;
+            Scanner scan = new Scanner(System.in) ;
 
+
+            // get three values from user
+            System.out.println("Please enter three integers and " +
+                    "I will compute their average");
 }

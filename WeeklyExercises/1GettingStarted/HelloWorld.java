fd810f0e2430952a1b25e3a675a54ca3f2c500e1
LKZ007 <l.kibale-zoko3989@student.leedsbeckett.ac.uk>
2025-02-06 11:07:17 +0000

    week2

--- WeeklyExercises/1GettingStarted/HelloWorld.java
+++ WeeklyExercises/1GettingStarted/HelloWorld.java
@@ -3,5 +3,11 @@ public class HelloWorld
     public static void main (String[] args)
     {
         System.out.println("Hello World!");
+        int length;
+        length = 10;
+
+
+
+
     }
 }
---
+++ WeeklyExercises/2DataAndExpressions/StudentGrade.java
@@ -0,0 +1,14 @@
+public class StudentGrade
+{
+    public static void main(String[] args)
+    {
+        System.out.println("///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==          Student Points          ==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+        System.out.println("Name            Lab     Bonus   Total");
+        System.out.println("Joe             43      7       50");
+        System.out.println("William         50      8       58");
+        System.out.println("Mary Sue        39      10      49");
+    }
+
+}
